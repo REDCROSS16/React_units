2a2f0507409fbdf42056e6e00940f0d6865760a2
REDCROSS16 <belkill@mail.ru>
2022-04-13 23:23:13 +0300

    unit_05

--- unit_01/src/Range.js
+++ unit_01/src/Range.js
@@ -2,16 +2,19 @@ import {useState} from "react";
 
 // компонент для вывода инпута range
 function Range (props) {
-
+    console.log('Компонент сработал');
     console.log(props)
     const [value, setValue] = useState(0);
+    const [newValue, setNewValue] = useState(0);
 
     function inputHandler (event) {
+        console.log('Функция сработала');
         setValue(event.target.value);
     }
 
     return (
-        <>
+        <> 
+        {console.log('Return сработал')}
             <input type='range' value={value} onChange={inputHandler} max={props.maxRange}></input>
             <p> {value} </p>
         </>
